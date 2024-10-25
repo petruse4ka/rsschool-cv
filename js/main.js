e3530fca42fd43f250abcd2ceaefb458dc11d691
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-10-25 20:04:10 +0200

    fix: add slider scroll update on window resize

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ const scrollRightButton = document.querySelector('.scroll-right');
 let currentIndex = 0;
 let itemWidth = sliderItems[0].offsetWidth;
 
+function updateItemWidth() {
+  itemWidth = sliderItems[0].offsetWidth;
+  slider.scrollLeft = currentIndex * itemWidth;
+}
+
 function updateButtonStates() {
   scrollLeftButton.classList.toggle('disabled', currentIndex === 0);
   scrollRightButton.classList.toggle('disabled', currentIndex >= sliderItems.length - 2);
@@ -15,14 +20,18 @@ function updateButtonStates() {
 
 scrollLeftButton.addEventListener('click', () => {
   currentIndex--;
-  slider.scrollLeft -= itemWidth;
+  slider.scrollLeft = currentIndex * itemWidth;
   updateButtonStates();
 });
 
 scrollRightButton.addEventListener('click', () => {
   currentIndex++;
-  slider.scrollLeft += itemWidth;
+  slider.scrollLeft = currentIndex * itemWidth;
   updateButtonStates();
+});
+
+window.addEventListener('resize', () => {
+  updateItemWidth();
 });
 
 updateButtonStates();
